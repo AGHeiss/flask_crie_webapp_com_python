6d177e175a2f7bbfa83782324201e2c079d5ea1e
AGHeiss <augustoheiss@gmail.com>
2024-04-24 21:13:14 -0300

    Aprendendo Flask com Python

--- aprendendo_flask.py
+++ aprendendo_flask.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 class Jogo:
     def __init__(self, nome, categoria, console):
@@ -6,14 +6,30 @@ class Jogo:
         self.categoria=categoria
         self.console=console
 
+jogo1 = Jogo('Minecraft', 'Sandbox', 'PC')
+jogo2 = Jogo('The Sims', 'Simulador de vida real', 'PC')
+jogo3 = Jogo('Age of Empires III', 'Estratégia em tempo real', 'PC')
+jogos = [jogo1, jogo2, jogo3]
+
+
 app = Flask(__name__)
 
-@app.route('/inicio')
-def ola():
-    jogo1 = Jogo('Minecraft', 'Sandbox', 'PC')
-    jogo2 = Jogo('The Sims', 'Simulador de vida real', 'PC')
-    jogo3 = Jogo('Age of Empires III', 'Estratégia em tempo real', 'PC')
-    jogos = [jogo1, jogo2, jogo3]
+@app.route('/home')
+def index():
     return render_template('aprendendo_flask.html', titulo = 'Jogos',lista=jogos)
 
-app.run()
+@app.route('/novo')
+def cadastro():
+    return render_template('cadastro.html', titulo = 'Novo Jogo')
+
+@app.route('/atualizar', methods=['POST',])
+def atualizar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console)
+    jogos.append(jogo)
+    return render_template('aprendendo_flask.html', titulo = 'Jogos', lista=jogos)
+
+
+app.run(debug=True)
