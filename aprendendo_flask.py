04b64f2db509479623e657719e541f04a2534148
AGHeiss <augustoheiss@gmail.com>
2024-05-01 21:13:35 -0300

    Aprendendo Flask com Python

--- aprendendo_flask.py
+++ aprendendo_flask.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, flash
 
 class Jogo:
     def __init__(self, nome, categoria, console):
@@ -13,13 +13,16 @@ jogos = [jogo1, jogo2, jogo3]
 
 
 app = Flask(__name__)
-
+app.secret_key = 'ABCD'
 @app.route('/home')
 def index():
     return render_template('aprendendo_flask.html', titulo = 'Jogos',lista=jogos)
 
 @app.route('/novo')
 def cadastro():
+    if 'usuario_logado' not in session or session['usuario_logado'] == None:
+        return redirect('/login')
+
     return render_template('cadastro.html', titulo = 'Novo Jogo')
 
 @app.route('/atualizar', methods=['POST',])
@@ -30,6 +33,27 @@ def atualizar():
     jogo = Jogo(nome, categoria, console)
     jogos.append(jogo)
     return redirect('/home')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/autenticar', methods=['POST',])
+def autenticar():
+    senha = request.form['senha']
+    if 'alohomora' == senha:
+        session['usuario_logado'] = request.form['usuario']
+        flash(request.form['usuario'] + ' logado com sucesso!')
+        return redirect('/home')
+    else:
+        flash('Senha incorreta!')
+        return redirect('/login')
+
+@app.route('/deslogar')
+def deslogar():
+    session['usuario_logado'] = None
+    flash('Usuário deslogado com sucesso')
+    return redirect('/login')
 
 
 app.run(debug=True)
